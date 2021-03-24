9e15f3eb969ce1e8b31506afddeebc1cd4dd12e2
Billy <bvb4ever1908@outlook.com>
2021-03-24 10:09:18 +0200

    First commit (3 Files)

---
+++ MainPage.php
@@ -0,0 +1,49 @@
+<!DOCTYPE php>
+<html>
+<head>
+
+<!--JQUERY LIBRARIES-->
+<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!--FONT-FAMILIES USED-->
+<link href='https://fonts.googleapis.com/css?family=Arbutus' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Amiko' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Advent Pro' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Architects Daughter' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Archivo' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Flamenco' rel='stylesheet'>
+
+<meta name="theme-color" content="rgb(190, 214, 148)">
+
+<link rel="stylesheet" type="text/css" href="Css/CssHome.css">
+
+	<title>Αρχική</title>
+
+</head>
+
+<body>
+	
+	<div id="headTag">
+		<div id="menuTag">
+			<button id="menuBtn">
+				<div id="menuBtnImg"></div>
+			</button>
+			<div id="botAlign">
+				<div id="botBtnsMenu">
+					<button id="openBtn">1</button>
+					<button id="openBtn">2</button>
+					<button id="openBtn">3</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="bodyTag">
+		<div id=""></div>
+	</div>
+
+	<script type="text/javascript" src="Js/HomeJSScript.js"></script>
+
+</body>
+</html>
